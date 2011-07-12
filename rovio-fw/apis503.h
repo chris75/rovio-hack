b954092649e91d136edad7b39401836324b8ef40
Chris75 <croue7@yahoo.com>
2011-07-12 20:34:34 +0200

    Added APIs

--- rovio-fw/apis503.h
+++ rovio-fw/apis503.h
@@ -5,7 +5,10 @@
 #define fw_AddHttpValue ((void (*)( void *pReturnXML, const char *pcString, const char *pcValue )) 0x0006C34C)
 
 //void prdAddTask(PRD_TASK_T *pHandle, void (*fnTask)(void *pArg), cyg_tick_count_t tTimeout, void *pArg);
-#define fw_prdAddTask (((void (*)( void *pHandle, void *pFunc, unsigned long timeout, void *pArg )) 0x000C9E70)  //## TO CHECK
+#define fw_prdAddTask (((void (*)( void *pHandle, void *pFunc, unsigned long timeout, void *pArg )) 0x000C9E70)  
 
 // void ledShowState_Ready() 
-#define fw_letShowStateready (((void (*)( )) 0x0001234) //## NOT FOUND YET
+#define fw_ledShowStateready (((void (*)( )) 0x0001234) //##
+
+// void ledShowState_Error() 
+#define fw_ledShowStateready (((void (*)( )) 0x000BDAEC) 
