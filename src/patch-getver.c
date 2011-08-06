d7b50d85f9977f241b394f23601f373f72fd1aef
Chris75 <croue7@yahoo.com>
2011-08-06 14:15:04 +0200

    Add code to detect RAM patch for rovio

--- src/patch-getver.c
+++ src/patch-getver.c
@@ -4,9 +4,21 @@
 /* int Config_GetVer(HTTPCONNECTION hConnection, LIST *pParamList, int iAction, XML *pReturnXML)*/
 int Config_GetVer(void *R0, void *R1, void *R2, void *R3 ) 
 {
-
-  /* Call a firmware function */
-  fw_AddHttpValue(R3,"MyGetVer","12345-12"); 
+  /* Check if PATCH was loaded in RAM if so call it */
+  if(    ((char *)(ROVIO_RAM_MALLOC_BASE_ADDR))[0] == 'P' 
+      && ((char *)(ROVIO_RAM_MALLOC_BASE_ADDR))[1] == 'A'
+      && ((char *)(ROVIO_RAM_MALLOC_BASE_ADDR))[2] == 'T'
+      && ((char *)(ROVIO_RAM_MALLOC_BASE_ADDR))[3] == 'C'
+    )
+  {
+    /* Call a firmware function */
+    fw_AddHttpValue(R3,"Patch called","."); 
+  }
+  else
+  {
+    /* Call a firmware function */
+    fw_AddHttpValue(R3,"Patch not found","!"); 
+  }
 
   return 0;
 }
